78cfc663d6156b9994678a0a0b4242d1fdf03cfe
joesuttondev <joesuttondevelopment@gmail.com>
2021-11-20 16:47:53 +0000

    Re-add TeamsController

---
+++ Controllers/TeamsController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using jsfootball_api.Models;
+
+namespace jsfootball_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly TeamsContext _context;
+
+        public TeamsController(TeamsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Teams
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Team>>> GetTeams()
+        {             
+            return await _context.Teams.ToListAsync();
+        }
+
+        // GET: api/Teams/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Team>> GetTeam(string id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return team;
+        }        
+    }
+}
